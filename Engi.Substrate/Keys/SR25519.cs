0ea08e85f51e2bea275bb06c1d82d64c2fa7342e
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-06-20 14:25:25 +0300

    fix: add osx intel/arm64 builds and dynamically load engi_crypto for supported platforms

--- Engi.Substrate/Keys/SR25519.cs
+++ Engi.Substrate/Keys/SR25519.cs
@@ -1,9 +1,55 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Engi.Substrate.Keys;
 
 internal static class SR25519
 {
+    static SR25519()
+    {
+        NativeLibrary.SetDllImportResolver(typeof(SR25519).Assembly,
+            (name, assembly, path) =>
+            {
+                string basePath = Path.GetDirectoryName(assembly.Location)!;
+
+                IntPtr Load(string filename)
+                {
+                    return NativeLibrary.Load(
+                        Path.Combine(basePath, "lib", filename));
+                }
+
+                if (name == "engi_crypto")
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        if (RuntimeInformation.OSArchitecture == Architecture.Arm64)
+                        {
+                            return Load("libengi_crypto_arm64.dylib");
+                        }
+
+                        return Load("libengi_crypto.dylib");
+                    }
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+                        && RuntimeInformation.OSArchitecture == Architecture.X64)
+                    {
+                        return Load("libengi_crypto.so");
+                    }
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                        && RuntimeInformation.OSArchitecture == Architecture.X64)
+                    {
+                        return Load("engi_crypto.dll");
+                    }
+
+                    throw new NotSupportedException(
+                        "The combination of OSArchitecture and OSPlatform is not supported.");
+                }
+
+                return IntPtr.Zero;
+            });
+    }
+
     [DllImport("engi_crypto",
         CharSet = CharSet.Auto,
         CallingConvention = CallingConvention.Cdecl,
