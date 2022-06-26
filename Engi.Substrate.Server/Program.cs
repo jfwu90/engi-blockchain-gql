2fce8477344539698d972dc11d73084b7ff4ba09
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-06-26 13:40:55 +0300

    fix: remove leftover debugger statement

--- Engi.Substrate.Server/Program.cs
+++ Engi.Substrate.Server/Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        System.Diagnostics.Debugger.Launch();
         var host = Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
