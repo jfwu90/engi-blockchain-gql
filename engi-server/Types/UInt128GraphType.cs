cd0ba981bcf417317b4acc0f4c72be58dd10737c
Georgios Diamantopoulos <georgiosd@zerotomvp.dev>
2022-08-22 15:26:49 +0300

    fix: throw if u128 contains a negative number

--- engi-server/Types/UInt128GraphType.cs
+++ engi-server/Types/UInt128GraphType.cs
@@ -14,6 +14,11 @@ public class UInt128GraphType : ScalarGraphType
             throw new InvalidDataException("Value exceeds limit of 128 bits.");
         }
 
+        if (parsed.Sign == -1)
+        {
+            throw new InvalidDataException("Negative value supplied for an unsigned type.");
+        }
+
         return parsed;
     }
 }
