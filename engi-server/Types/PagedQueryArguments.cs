45da586f6a1f06e8f7bc41bfd30f8abc01772ea1
Georgios Diamantopoulos <georgiosd@zerotomvp.dev>
2022-08-24 17:29:25 +0300

    fix: define defaults

--- engi-server/Types/PagedQueryArguments.cs
+++ engi-server/Types/PagedQueryArguments.cs
@@ -5,8 +5,8 @@ namespace Engi.Substrate.Server.Types;
 public class PagedQueryArguments
 {
     [Range(0, int.MaxValue)]
-    public int Skip { get; set; }
+    public int Skip { get; set; } = 0;
 
     [Range(10, 100)]
-    public int Limit { get; set; }
+    public int Limit { get; set; } = 25;
 }
