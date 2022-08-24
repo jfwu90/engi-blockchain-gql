cbc3ba5a0afb2fb4b8c87347ed717eee5bc8cf0a
Georgios Diamantopoulos <georgiosd@zerotomvp.dev>
2022-08-24 17:28:53 +0300

    fix: require sender secret

--- engi-substrate/SignedExtrinsicArgumentsBase.cs
+++ engi-substrate/SignedExtrinsicArgumentsBase.cs
@@ -1,7 +1,10 @@
-﻿namespace Engi.Substrate;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Engi.Substrate;
 
 public abstract class SignedExtrinsicArgumentsBase : ISignedExtrinsic
 {
+    [Required]
     public string SenderSecret { get; set; } = null!;
 
     public byte Tip { get; set; }
