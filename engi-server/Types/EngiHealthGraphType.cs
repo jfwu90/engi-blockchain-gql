2872a7ed1a955750fc9ecb96011e27bc74f9546b
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-10-04 21:29:20 +0300

    fix: allow null for nullable props

--- engi-server/Types/EngiHealthGraphType.cs
+++ engi-server/Types/EngiHealthGraphType.cs
@@ -8,11 +8,11 @@ public class EngiHealthGraphType : ObjectGraphType<EngiHealth>
     {
         Description = "A type containing information about the chain and its current health status.";
 
-        Field(x => x.Chain)
+        Field(x => x.Chain, nullable: true)
             .Description("The name of the chain.");
-        Field(x => x.NodeName)
+        Field(x => x.NodeName, nullable: true)
             .Description("The name of the node.");
-        Field(x => x.Version)
+        Field(x => x.Version, nullable: true)
             .Description("The chain version.");
         Field(x => x.Status)
             .Description("The current status of the chain.");
