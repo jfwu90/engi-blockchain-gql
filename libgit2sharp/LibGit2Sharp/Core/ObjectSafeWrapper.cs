15cf78c3a574102563e817871964f02ec025384b
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-10-18 20:08:06 +0300

    fix: temporary import of libgit2sharp until PR #1986 is merged
    https://github.com/libgit2/libgit2sharp/pull/1986

---
+++ libgit2sharp/LibGit2Sharp/Core/ObjectSafeWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using LibGit2Sharp.Core.Handles;
+
+namespace LibGit2Sharp.Core
+{
+    internal class ObjectSafeWrapper : IDisposable
+    {
+        private readonly ObjectHandle objectPtr;
+
+        public unsafe ObjectSafeWrapper(ObjectId id, RepositoryHandle handle, bool allowNullObjectId = false, bool throwIfMissing = false)
+        {
+            Ensure.ArgumentNotNull(handle, "handle");
+
+            if (allowNullObjectId && id == null)
+            {
+                objectPtr = new ObjectHandle(null, false);
+            }
+            else
+            {
+                Ensure.ArgumentNotNull(id, "id");
+                objectPtr = Proxy.git_object_lookup(handle, id, GitObjectType.Any);
+            }
+
+            if (objectPtr == null && throwIfMissing)
+            {
+                throw new NotFoundException($"No valid git object identified by '{id}' exists in the repository.");
+            }
+        }
+
+        public ObjectHandle ObjectPtr => objectPtr;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            objectPtr.SafeDispose();
+        }
+    }
+}
