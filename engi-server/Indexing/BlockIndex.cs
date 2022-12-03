d6670e95dc4091acb7ee9392b3cff57c3b40c0ad
Georgios Diamantopoulos <georgiosd@gmail.com>
2022-12-03 15:40:48 +0200

    feat: add index to monitor block indexing

---
+++ engi-server/Indexing/BlockIndex.cs
@@ -0,0 +1,32 @@
+using Raven.Client.Documents.Indexes;
+
+namespace Engi.Substrate.Server.Indexing;
+
+public class BlockIndex : AbstractIndexCreationTask<ExpandedBlock>
+{
+    public class Result
+    {
+        public ulong Number { get; set; }
+
+        public string Hash { get; set; } = null!;
+
+        public DateTime? IndexedOn { get; set; }
+
+        public string? SentryId { get; set; }
+    }
+
+    public BlockIndex()
+    {
+
+        Map = blocks => from block in blocks
+            select new Result
+            {
+                Number = block.Number,
+                Hash = block.Hash,
+                IndexedOn = block.IndexedOn,
+                SentryId = block.SentryId
+            };
+
+        Priority = IndexPriority.Low;
+    }
+}
