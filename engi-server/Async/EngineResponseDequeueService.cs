86ce5f482da5339c3bbf6017280e8945efc7d7b1
TJ Sharp <tjsharp@TJs-MacBook-Pro-2.local>
2023-04-19 18:52:33 -0400

    Add more print

--- engi-server/Async/EngineResponseDequeueService.cs
+++ engi-server/Async/EngineResponseDequeueService.cs
@@ -47,6 +47,11 @@ public class EngineResponseDequeueService : BackgroundService
 
         var sqs = new AmazonSQSClient(credentials, config);
 
+        if (string.IsNullOrEmpty(engiOptions.AssumeRole) {
+            logger.LogInformation("Assume role is empty, using fallback");
+        } else {
+            logger.LogInformation("Using assume role");
+        }
         logger.LogInformation("Processing queue messages from engine. queue={queue}", engiOptions.EngineOutputQueueUrl);
         while (!stoppingToken.IsCancellationRequested)
         {
