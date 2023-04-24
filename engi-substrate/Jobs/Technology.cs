077b3238da9c7c61ebd5fce5ea4210085b1dd234
TJ Sharp <tjsharp@TJs-MacBook-Pro-2.local>
2023-04-24 16:14:39 -0400

    Fix enum, add assume role

--- engi-substrate/Jobs/Technology.cs
+++ engi-substrate/Jobs/Technology.cs
@@ -5,5 +5,15 @@ public enum Technology
     CSharp = 0,
     Python = 1,
     JavaScript = 2,
-    Rust = 3
+    TypeScript = 3,
+    Rust = 4,
+    Solidity = 5,
+    Solang = 6,
+    Ink = 7,
+    Substrate = 8,
+    CPP = 9,
+    C = 10,
+    SameStory = 11,
+    CSS = 12,
+    Docker = 13,
 }
